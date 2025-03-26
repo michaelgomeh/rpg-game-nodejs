15108a52a0735d55c3e104794f19d5eb7d7fa56e
Michael Gomeh <gomehmichael@gmail.com>
2025-03-26 15:23:22 +0200

    add inventory and player logs

--- player.js
+++ player.js
@@ -2,7 +2,16 @@ class Player {
 	constructor(name) {
 		this.name = name;
 		this.hp = 10;
+		this.inventory = ['Letter from mom'];
 		console.log(`welcome to Hell, ${name}`);
+	}
+
+	logStats() {
+		console.log(`Mighty ${this.name} has ${hp} HP`);
+	}
+
+	logInventory() {
+		console.log(`${this.name}'s inventory includes: ${inventory.join(', ')}`);
 	}
 }
 
