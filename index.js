6eacf9f05df4b1e6c58623ec091cbdd5e3aec72a
Michael Gomeh <gomehmichael@gmail.com>
2025-03-26 14:11:48 +0200

    add basic promt

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+import inquirer from 'inquirer';
 
 console.log('Start Game!');
+
+const choice = inquirer.prompt({
+	type: 'list',
+	name: 'action',
+	message: 'Choose your next action:',
+	choices: ['Draw a card', 'Explore the current room'],
+});
+
+console.log(choice);
