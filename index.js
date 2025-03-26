c8a80ad805dc991dce84606c09dacf9643ba79fb
Michael Gomeh <gomehmichael@gmail.com>
2025-03-26 14:26:41 +0200

    handle choices ststem

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 #!/usr/bin/env node
 import inquirer from 'inquirer';
 
+const handleChoice = (choice) => {
+	switch (choice) {
+		case 'Exit Game':
+			console.log('\nGoodbye!');
+			process.exit();
+			break;
+
+		case 'Draw a card':
+			break;
+
+		case 'View Inventory':
+			break;
+
+		default:
+			break;
+	}
+};
+
+const showNextTurnMenu = async () => {
+	const choice = await inquirer.prompt({
+		type: 'list',
+		name: 'action',
+		message: 'Choose your next action:',
+		choices: ['Draw a card', 'View Inventory', 'Exit Game'],
+	});
+
+	handleChoice(choice);
+};
+
 console.log('Start Game!');
 
-const choice = inquirer.prompt({
-	type: 'list',
-	name: 'action',
-	message: 'Choose your next action:',
-	choices: ['Draw a card', 'Explore the current room'],
-});
-
-console.log(choice);
+showNextTurnMenu();
